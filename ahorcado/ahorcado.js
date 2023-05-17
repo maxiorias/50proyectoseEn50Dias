9dde79399be6fb2d0333d1ba441cf06f8bd04476
Maximiliano Orias <maxiorias@gmail.com>
2023-05-17 12:09:18 -0300

    funciones

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -1,4 +1,9 @@
+const palabraSecreta = 'JAVASCRIPT';
+let palabraAdivinada = '_ _ _ _ _ _ _ _ _ _';
+let intentosRestantes = 6;
 
-var palabraSecreta = 'JAVASCRIPT';
-    var palabraAdivinada = '_ _ _ _ _ _ _ _ _ _';
-    var intentosRestantes = 6;
+// con esta funcion actualizo la palabra advinada en la interfaz
+function actualizarPalabraAdivinada () {
+    const palabraElemento = document.querySelector('.palabra');
+    palabraElemento.textContent = palabraAdivinada;
+}
