5fc11cba6abe16ae7600acb5bfad4143e60d5975
Maximiliano Orias <maxiorias@gmail.com>
2023-05-20 13:20:05 -0300

    mathfloor mathrandom

--- generador de contraseñas/generador.js
+++ generador de contraseñas/generador.js
@@ -2,3 +2,7 @@ const generarContrasena = () => {
     const caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let contrasena = '';
 }
+
+for (let i=0; i < 8; i++){
+    const indice = Math.floor(Math.random()* caracteres.length)
+}
