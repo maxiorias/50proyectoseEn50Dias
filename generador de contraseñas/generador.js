eecf62c9138fb95a4d4fbcf20e4c08991c56c990
Maximiliano Orias <maxiorias@gmail.com>
2023-05-20 13:25:11 -0300

    js

--- generador de contraseñas/generador.js
+++ generador de contraseñas/generador.js
@@ -1,8 +1,16 @@
 const generarContrasena = () => {
     const caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let contrasena = '';
+    
+    for (let i=0; i < 8; i++){
+        const indice = Math.floor(Math.random()* caracteres.length);
+        contrasena += caracteres.charAt(indice);
+    }
+    
+
+    return contrasena;
 }
 
-for (let i=0; i < 8; i++){
-    const indice = Math.floor(Math.random()* caracteres.length)
-}
+const nuevaContrasena = generarContrasena();
+console.log(nuevaContrasena);
+
