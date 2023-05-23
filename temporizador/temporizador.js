2763f291b65676aa0598734dfeedf04f6ff795ab
Maximiliano Orias <maxiorias@gmail.com>
2023-05-22 21:39:37 -0300

    funcion actualizar tempo

---
+++ temporizador/temporizador.js
@@ -0,0 +1,10 @@
+// fecha y hora objetivo
+
+const fechaObjetivo = new Date('2023-06-21T23:59:59');
+
+// funcion para actualizar el temporizador
+
+const actualizarCuentaRegresiva = () => {
+    const fechaActual = new Date ();
+    const tiempoRestante = fechaObjetivo - fechaActual;
+}
