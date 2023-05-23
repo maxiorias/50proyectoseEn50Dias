628f1da812c2f9f5e094d64fd6c7fc2f4a3f4b55
Maximiliano Orias <maxiorias@gmail.com>
2023-05-22 22:04:09 -0300

    funciones

--- temporizador/temporizador.js
+++ temporizador/temporizador.js
@@ -1,13 +1,20 @@
-// fecha y hora objetivo
-
 const fechaObjetivo = new Date('2023-06-21T23:59:59');
 
-// funcion para actualizar el temporizador
-
 const actualizarCuentaRegresiva = () => {
-    const fechaActual = new Date ();
-    const tiempoRestante = fechaObjetivo - fechaActual;
-}
+  const fechaActual = new Date();
+  const tiempoRestante = fechaObjetivo - fechaActual;
 
-const dias = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
-const horas = Math.floor((tiempoRestante %(1000 * 60 * 60 * 24))) /(1000 * 60 * 60));
+  const dias = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((tiempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((tiempoRestante % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((tiempoRestante % (1000 * 60)) / 1000);
+
+  document.getElementById('cuenta-regresiva').innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+
+  if (tiempoRestante <= 0) {
+    clearInterval(temporizador);
+    document.getElementById('cuenta-regresiva').innerHTML = '¡Tiempo finalizado!';
+  }
+};
+
+const temporizador = setInterval(actualizarCuentaRegresiva, 1000);
